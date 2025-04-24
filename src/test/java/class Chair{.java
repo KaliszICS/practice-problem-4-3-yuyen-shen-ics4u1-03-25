98fb02456f279eeb6aa00589c85707a26e6bb85d
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-04-24 18:30:46 +0000

    yuyen

--- src/test/java/class Chair{.java
+++ src/test/java/class Chair{.java
@@ -1,27 +1,54 @@
+/**
+ * chair information
+ * @author Yuyen
+ */
+
 class Chair{
     
     private int legs;
     private String material;
+
+    /**
+     * a set of information of the chair
+     * @param legs the amount of legs the chair has
+     * @param material the material that the chair is made of
+     */
 
     public Chair() {
         this.legs = 4;
         this.material = "wood";
     }
 
+    /**
+     * @param legs1 the amount of legs that will replace its original number of legs
+     */
     public Chair(int legs1) {
         this.legs = legs1;
         this.material = "wood";
     }
 
+    /**
+     * 2 variables of the chair
+     * @param legs2 the new amount of legs that will replace the original value of legs
+     * @param material2 the new material that will replace the original material
+     */
     public Chair(int legs2, String material2) {
         this.legs = legs2;
         this.material = material2;
     }
 
+    /**
+     * 
+     * @return the amount of legs
+     */
     public int getLegs() {
         return this.legs;
     }
 
+    /**
+     * 
+     * @return the material of the chair
+     */
     public String getMaterial() {
         return this.material;
     }
--- src/test/java/class Rectangle {.java
+++ src/test/java/class Rectangle {.java
@@ -1,27 +1,54 @@
+/**
+ * rectangle information
+ * @author Yuyen
+ */
+
 class Rectangle {
 
     private double length;
     private double width;
+
+    /**
+     * @param length the length of the rectangle
+     * @param width the width of the rectangle
+     */
 
     public Rectangle() {
         this.length = 4.0;
         this.width = 8.0;
     }
 
+    /**
+     * 
+     * @param length1 the new length that will replace the original length
+     */
     public Rectangle(double length1) {
         this.length = length1;
         this.width = 8.0;
     }
 
+    /**
+     * 
+     * @param length2 the new length that will replace the original length
+     * @param width2 the new width that will replace the original width
+     */
     public Rectangle(double length2, double width2){
         this.length = length2;
         this.width = width2;
     }
 
+    /**
+     * 
+     * @return the length of the rectangle
+     */
     public double getLength() {
         return this.length;
     }
 
+    /**
+     * 
+     * @return the width of the rectangle
+     */
     public double getWidth() {
         return this.width;
     }
